57d56148168e304ced5536d39a71df6f2249292f
Hack Reactor Student <student@hackreactor.com>
2017-09-14 19:59:18 -0700

    Refactoring to move middleware functions to separate file

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,45 +1,36 @@
 /* Import node's http module: */
 const http = require('http');
 const handler = require('./request-handler');
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
 const results = require('./messages');
 const bodyParser = require('body-parser');
+const router = express.Router();
+const handlePOST = handler.handlePOST;
+
+console.log('handlePOST is', handlePOST);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /* serves main page */
-app.get("/", function(req, res) {
-  console.log('dirname is', __dirname + '/../client');
+app.get('/', function(req, res) {
   res.sendFile(path.resolve('../client/index.html'));
 });
 
-
-
-app.post("/classes/messages", function(req, res) {
-  console.log('request body is', Object.keys(req.body)[0]);
-  const resultObj = JSON.parse(Object.keys(req.body)[0]);
-  resultObj.createdAt = new Date();
-  resultObj.objectId = results.length ? results[results.length - 1].objectId + 1 : 1;
-  results.push(resultObj);
-  const { method, url } = req;
-  const responseBody = { method, url };
-  responseBody.body = resultObj;
-  res.json(responseBody);
+app.post('/classes/messages', function(req, res) {
+  // const resultObj = JSON.parse(Object.keys(req.body)[0]);
+  // resultObj.createdAt = new Date();
+  // resultObj.objectId = results.length ? results[results.length - 1].objectId + 1 : 1;
+  // results.push(resultObj);
+  // const { method, url } = req;
+  // const responseBody = { method, url };
+  // responseBody.body = resultObj;
+  res.json(handlePOST(req, res));
 });
 
-
-
-
-
-
-
-
-
-
-app.get("/classes/messages*", function(req, res) { //TODO: Handle queries
+app.get('/classes/messages*', function(req, res) { //TODO: Handle queries
   const query = req.query;
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -57,101 +48,19 @@ app.get("/classes/messages*", function(req, res) { //TODO: Handle queries
 });
 
 /* serves all the static files */
-app.get(/^(.+)$/, function(req, res){
-  console.log('static file request : ' + req.params[0]);
+app.get(/^(.+)$/, function(req, res) {
   res.sendFile(path.resolve('../client') + req.params[0]);
 });
 
 
-app.options("/*", function(req, res, next){
-  console.log('Asking for options');
+app.options('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.send(200);
 });
 
-
-
-var port = 3000;
+const port = 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
-
-
-
-
-// if (path === '/' || path.match(/\.(?=css$|js$|gif$)/g) !== null) {
-//   let base = '../client';
-//   base += path === '/' ? '/index.html' : path;
-//   fs.readFile(base, function (err, data) {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     response.end(data.toString());
-//   });
-// } else
-// if (!url.startsWith("/classes/messages")) {
-//   console.log('WRONG URL', url);
-//   response.writeHead(404, headers);
-//   response.end();
-// } else if (method === 'GET' || method === "OPTIONS") {
-//   response.writeHead(200, headers);
-//   let responseBody;
-//   if (queryArray.query['order'] && queryArray.query['order'] === '-createdAt') {
-//     const reverseResults = results.slice().reverse();
-//     responseBody = { method, url };
-//     responseBody.results = reverseResults;
-//   } else {
-//     responseBody = { method, url, results };
-//   }
-//   response.end(JSON.stringify(responseBody));
-// } else if (method === 'POST') {
-//   let body = [];
-//
-//   request.on('data', (chunk) => {
-//     body.push(chunk);
-//   });
-//
-//   request.on('error', (err) => {
-//     // This prints the error message and stack trace to `stderr`.
-//     console.error(err.stack);
-//     response.end('Error 404');
-//   });
-//
-//   request.on('end', () => {
-//     const resultObj = JSON.parse(body.toString());
-//     resultObj.createdAt = new Date();
-//     resultObj.objectId = results.length ? results[results.length - 1].objectId + 1 : 1;
-//     results.push(resultObj);
-//     response.writeHead(201, headers);
-//     const responseBody = { method, url };
-//     responseBody.body = resultObj;
-//     response.end(JSON.stringify(responseBody));
-//   });
-// } else if (method === 'DELETE') { //NOTE: To delete, pass in object id url?objectId=[num]
-//   let deletedMessage;
-//   let isFound = false;
-//   if (queryArray.query['objectId']) {
-//     const targetId = Number(queryArray.query['objectId']);
-//     for (let i = 0; i < results.length; i++) {
-//       if (results[i].objectId === targetId) {
-//         deletedMessage = results.splice(i, 1);
-//         isFound = true;
-//       }
-//     }
-//     if (isFound) {
-//       response.writeHead(201, headers);
-//       const responseBody = { method, url };
-//       responseBody.body = deletedMessage;
-//       response.end(JSON.stringify(responseBody));
-//     } else {
-//       response.writeHead(400, headers);
-//       response.end();
-//     }
-//   } else {
-//     response.writeHead(400, headers);
-//     response.end();
-//   }
-//
-// }
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,150 +1,25 @@
-// /*************************************************************
-//
-// You should implement your request handler function in this file.
-//
-// requestHandler is already getting passed to http.createServer()
-// in basic-server.js, but it won't work as is.
-//
-// You'll have to figure out a way to export this function from
-// this file and include it in basic-server.js so that it actually works.
-//
-// *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-//
-// **************************************************************/
-//
-// var defaultCorsHeaders = {
-//   'access-control-allow-origin': '*',
-//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//   'access-control-allow-headers': 'content-type, accept',
-//   'access-control-max-age': 10, // Seconds.
-//
-// };
-//
-// const results = require('./messages');
-// const urlMethods = require('url');
-// const fs = require("fs");
-//
-//
-//
-// var requestHandler = function(request, response) {
-//
-//   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-//
-//   const { method, url } = request; //Get request info
-//   var headers = defaultCorsHeaders;
-//   headers['Content-Type'] = 'application/json';
-//
-//   const queryArray = urlMethods.parse(url, true);
-//   const path = queryArray.pathname;
-//
-//
-//
-//
-//
-//   // if (path === '/' || path.match(/\.(?=css$|js$|gif$)/g) !== null) {
-//   //   let base = '../client';
-//   //   base += path === '/' ? '/index.html' : path;
-//   //   fs.readFile(base, function (err, data) {
-//   //     if (err) {
-//   //       return console.error(err);
-//   //     }
-//   //     response.end(data.toString());
-//   //   });
-//   // } else
-//   // if (!url.startsWith("/classes/messages")) {
-//   //   console.log('WRONG URL', url);
-//   //   response.writeHead(404, headers);
-//   //   response.end();
-//   // } else if (method === 'GET' || method === "OPTIONS") {
-//   //   response.writeHead(200, headers);
-//   //   let responseBody;
-//   //   if (queryArray.query['order'] && queryArray.query['order'] === '-createdAt') {
-//   //     const reverseResults = results.slice().reverse();
-//   //     responseBody = { method, url };
-//   //     responseBody.results = reverseResults;
-//   //   } else {
-//   //     responseBody = { method, url, results };
-//   //   }
-//   //   response.end(JSON.stringify(responseBody));
-//   // } else if (method === 'POST') {
-//   //   let body = [];
-//   //
-//   //   request.on('data', (chunk) => {
-//   //     body.push(chunk);
-//   //   });
-//   //
-//   //   request.on('error', (err) => {
-//   //     // This prints the error message and stack trace to `stderr`.
-//   //     console.error(err.stack);
-//   //     response.end('Error 404');
-//   //   });
-//   //
-//   //   request.on('end', () => {
-//   //     const resultObj = JSON.parse(body.toString());
-//   //     resultObj.createdAt = new Date();
-//   //     resultObj.objectId = results.length ? results[results.length - 1].objectId + 1 : 1;
-//   //     results.push(resultObj);
-//   //     response.writeHead(201, headers);
-//   //     const responseBody = { method, url };
-//   //     responseBody.body = resultObj;
-//   //     response.end(JSON.stringify(responseBody));
-//   //   });
-//   // } else if (method === 'DELETE') { //NOTE: To delete, pass in object id url?objectId=[num]
-//   //   let deletedMessage;
-//   //   let isFound = false;
-//   //   if (queryArray.query['objectId']) {
-//   //     const targetId = Number(queryArray.query['objectId']);
-//   //     for (let i = 0; i < results.length; i++) {
-//   //       if (results[i].objectId === targetId) {
-//   //         deletedMessage = results.splice(i, 1);
-//   //         isFound = true;
-//   //       }
-//   //     }
-//   //     if (isFound) {
-//   //       response.writeHead(201, headers);
-//   //       const responseBody = { method, url };
-//   //       responseBody.body = deletedMessage;
-//   //       response.end(JSON.stringify(responseBody));
-//   //     } else {
-//   //       response.writeHead(400, headers);
-//   //       response.end();
-//   //     }
-//   //   } else {
-//   //     response.writeHead(400, headers);
-//   //     response.end();
-//   //   }
-//   //
-//   // }
-//
-//   response.end('sad');
-//
-// };
-//
-//
-// exports.requestHandler = requestHandler;
-//
-//
-//
-//
-//
-//
-// /* serves main page */
-// app.get("/", function(req, res) {
-//    res.sendfile('../client.index.html')
-// });
-//
-// //  app.post("/user/add", function(req, res) {
-// //  /* some server side logic */
-// //  res.send("OK");
-// //  });
-// //
-// // /* serves all the static files */
-// // app.get(/^(.+)$/, function(req, res){
-// //     console.log('static file request : ' + req.params);
-// //     res.sendfile( __dirname + req.params[0]);
-// // });
-// //
-// // var port = process.env.PORT || 5000;
-// // app.listen(port, function() {
-// //   console.log("Listening on " + port);
-// // });
+const results = require('./messages');
+
+
+const handlePOST = (req, res) => {
+  const resultObj = JSON.parse(Object.keys(req.body)[0]);
+  resultObj.createdAt = new Date();
+  resultObj.objectId = results.length ? results[results.length - 1].objectId + 1 : 1;
+  results.push(resultObj);
+  const { method, url } = req;
+  const responseBody = { method, url };
+  responseBody.body = resultObj;
+  return responseBody;
+};
+
+
+
+
+
+
+
+
+
+
+
+exports.handlePOST = handlePOST;
